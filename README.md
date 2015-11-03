e2cbf65cad80f6e3952bbfa990e94ba6d1bcc2f0
Chaoyi Huang <joehuang@huawei.com>
2015-11-03 14:11:46 +0800

    Initial Kingbird framework code base
    Fisrt patch to implement the framework of Kingbird, with keystone authentication integrated.
    Includeing service of Kingbird-API, Kingbird-JobDaemon, Kingbird-JobWorker
    
    Blueprint: https://blueprints.launchpad.net/kingbird/+spec/kingbird-framework
    
    Change-Id: I62d7e8cbc9ab99e9386b4115a5a7bc20d8448b6d
    Signed-off-by: Chaoyi Huang <joehuang@huawei.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kingbird
+
+Centralised service for multi-region OpenStack deployments.
+
+Kingbird is an centralized OpenStack service that provides resource operation and management across multiple OpenStack instances in a multi-region OpenStack deployment. This service is part of the OPNFV Multisite project that intends to address the use cases related to distributed cloud environments.
+
+Kingbird provides features like centralized quota management, centralized view for distributed virtual resources, global view for tenant level IP/MAC address space management, synchronisation of ssh keys, images, flavors, security groups, etc. across regions.
+
+## Project Resources
+- Project status, bugs, and blueprints are tracked on [Launchpad](https://launchpad.net/kingbird)
+- Additional resources are linked from the project [Wiki](https://wiki.openstack.org/wiki/kingbird) page
---
+++ kingbird/jobworker/jwservice.py
@@ -0,0 +1,80 @@
+# Copyright 2015 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_service import service as srv
+
+from kingbird.common.i18n import _
+from kingbird.common.serializer import KingbirdSerializer as Serializer
+from kingbird.common.service import Service
+from kingbird.common import topics
+
+from kingbird.jobworker.jwmanager import JWManager
+
+_TIMER_INTERVAL = 30
+_TIMER_INTERVAL_MAX = 60
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+
+
+class JWService(Service):
+    def __init__(self, host, binary, topic, manager, report_interval=None,
+                 periodic_enable=None, periodic_fuzzy_delay=None,
+                 periodic_interval_max=None, serializer = None,
+                 *args, **kwargs):
+        super(JWService, self).__init__(host, binary, topic, manager,
+                                        report_interval, periodic_enable,
+                                        periodic_fuzzy_delay,
+                                        periodic_interval_max, serializer,
+                                        *args, **kwargs)
+
+
+def create_service():
+
+    LOG.debug(_('create job worker server'))
+
+    jwmanager = JWManager()
+    jwservice = JWService(
+        host=CONF.host,
+        binary="job_worker",
+        topic=topics.TOPIC_JOBWORKER,
+        manager=jwmanager,
+        periodic_enable=True,
+        report_interval=_TIMER_INTERVAL,
+        periodic_interval_max=_TIMER_INTERVAL_MAX,
+        serializer=Serializer()
+    )
+
+    jwservice.start()
+
+    return jwservice
+
+
+_launcher = None
+
+
+def serve(jwservice, workers=1):
+    global _launcher
+    if _launcher:
+        raise RuntimeError(_('serve() can only be called once'))
+
+    _launcher = srv.launch(CONF, jwservice, workers=workers)
+
+
+def wait():
+    _launcher.wait()

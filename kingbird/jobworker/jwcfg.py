9c5c52384b3967e9b2eb66777c8c5228fe0a3647
Chaoyi Huang <joehuang@huawei.com>
2015-11-09 15:43:21 +0800

    Initial Kingbird framework code base ( part3:jobworker )
    Fisrt patch to implement the framework of Kingbird, this is
    the part3 for jobworker, which receive rpc from jobdaemon
    service, and return messge to the jobdaemon request with
    helloworld example. After this patch, Kingbird API, jobdaemon,
    jobworker can work together through RPC message.
    
    Change-Id: I6c82b72e6d91cf328171bcf9ed2df66809b93047
    Blueprint: https://blueprints.launchpad.net/kingbird/+spec/kingbird-framework

--- kingbird/jobworker/jwcfg.py
+++ kingbird/jobworker/jwcfg.py
@@ -19,43 +19,29 @@ Routines for configuring kingbird, largely copy from Neutron
 
 import sys
 
-
 from oslo_config import cfg
 from oslo_log import log as logging
+
+from kingbird.common import rpc
 
 from kingbird.common.i18n import _
 from kingbird.common.i18n import _LI
 
 
 # from kingbird import policy
-from kingbird.common import rpc
 from kingbird.common import version
 
 LOG = logging.getLogger(__name__)
 
 common_opts = [
-    cfg.StrOpt('bind_host', default='0.0.0.0',
-               help=_("The host IP to bind to")),
-    cfg.IntOpt('bind_port', default=8118,
-               help=_("The port to bind to")),
-    cfg.IntOpt('api_workers', default=2,
-               help=_("number of api workers")),
-    cfg.StrOpt('api_paste_config', default="api-paste.ini",
-               help=_("The API paste config file to use")),
-    cfg.StrOpt('api_extensions_path', default="",
-               help=_("The path for API extensions")),
-    cfg.StrOpt('auth_strategy', default='keystone',
-               help=_("The type of authentication to use")),
-    cfg.BoolOpt('allow_bulk', default=True,
-                help=_("Allow the usage of the bulk API")),
-    cfg.BoolOpt('allow_pagination', default=False,
-                help=_("Allow the usage of the pagination")),
-    cfg.BoolOpt('allow_sorting', default=False,
-                help=_("Allow the usage of the sorting")),
-    cfg.StrOpt('pagination_max_limit', default="-1",
-               help=_("The maximum number of items returned in a single "
-                      "response, value was 'infinite' or negative integer "
-                      "means no limit")),
+    cfg.StrOpt('host', default='kingbird.jwhost',
+               help=_("The host name for RPC server")),
+    cfg.IntOpt('workers', default=2,
+               help=_("number of workers")),
+    cfg.StrOpt('state_path',
+               default='/var/lib/kingbird',
+               deprecated_name='pybasedir',
+               help="Top-level directory for maintaining kingbird's state"),
 ]
 
 
@@ -67,7 +53,7 @@ def init(args, **kwargs):
     # auth.register_conf_options(cfg.CONF)
     logging.register_options(cfg.CONF)
 
-    cfg.CONF(args=args, project='kingbird',
+    cfg.CONF(args=args, project='kingbird.jobworker',
              version='%%(prog)s %s' % version.version_info.release_string(),
              **kwargs)
 
@@ -76,7 +62,7 @@ def init(args, **kwargs):
 
 def setup_logging():
     """Sets up the logging options for a log with supplied name."""
-    product_name = "kingbird"
+    product_name = "kingbird.jobworker"
     logging.setup(cfg.CONF, product_name)
     LOG.info(_LI("Logging enabled!"))
     LOG.info(_LI("%(prog)s version %(version)s"),
@@ -93,10 +79,3 @@ def reset_service():
 
     # TODO(joehuang) enforce policy later
     # policy.refresh()
-
-
-def test_init():
-    # Register the configuration options
-    cfg.CONF.register_opts(common_opts)
-    logging.register_options(cfg.CONF)
-    setup_logging()

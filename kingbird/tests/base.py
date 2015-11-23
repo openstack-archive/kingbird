f295a7c5ee36a6cae3d0a2c7128d7203586be778
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2015-11-23 15:04:37 +0100

    Add database layer for the quota part
    Added a db abstraction for quota sync methods.
    Implementation currently is based on sqlalchemy.
    Added database migration enablement.
    Added the db unit tests.
    Further enhancement to the db API will be added
    in a separate comment.
    
    Change-Id: I56284919ad54d2359fa03d476cc89a4acdc68122

--- kingbird/tests/base.py
+++ kingbird/tests/base.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2010-2011 OpenStack Foundation
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# Copyright (c) 2015 Ericsson AB
+# All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -15,9 +13,52 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from kingbird.db import api as db_api
+from kingbird.tests import utils
+from oslo_config import cfg
+from oslo_db import options
 from oslotest import base
+import sqlalchemy
+
+get_engine = db_api.get_engine
 
 
-class TestCase(base.BaseTestCase):
-
+class KingbirdTestCase(base.BaseTestCase):
     """Test case base class for all unit tests."""
+
+
+class KingbirdDbTestCase(base.BaseTestCase):
+    def setup_dummy_db(self):
+        options.cfg.set_defaults(options.database_opts,
+                                 sqlite_synchronous=False)
+        options.set_defaults(cfg.CONF, connection="sqlite://",
+                             sqlite_db='kingbird.db')
+        engine = get_engine()
+        db_api.db_sync(engine)
+        engine.connect()
+
+    def reset_dummy_db(self):
+        engine = get_engine()
+        meta = sqlalchemy.MetaData()
+        meta.reflect(bind=engine)
+
+        for table in reversed(meta.sorted_tables):
+            if table.name == 'migrate_version':
+                continue
+            engine.execute(table.delete())
+
+    def create_quota_limit(self, ctxt, **kwargs):
+        values = {
+            'project_id': utils.UUID1,
+            'resource': "ram",
+            'limit': 10,
+        }
+        values.update(kwargs)
+        return db_api.quota_create(ctxt, **values)
+
+    def setUp(self):
+        super(KingbirdDbTestCase, self).setUp()
+
+        self.setup_dummy_db()
+
+        self.addCleanup(self.reset_dummy_db)

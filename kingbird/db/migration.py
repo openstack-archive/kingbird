3f1b45105260e68985b79e34ade0e3407dfcc9f3
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2015-11-23 15:04:37 +0100

    Add database layer for the quota part
    Added a db abstraction for quota sync methods.
    Implementation currently is based on sqlalchemy.
    Added database migration enablement.
    Added the db unit tests.
    Further enhancement to the db API will be added
    in a separate comment.
    
    Change-Id: I56284919ad54d2359fa03d476cc89a4acdc68122

---
+++ kingbird/db/migration.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2015 Ericsson AB
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Database setup and migration commands."""
+
+from kingbird.db.sqlalchemy import migration
+
+IMPL = migration
+
+
+def db_sync(version=None, database='main'):
+    """Migrate the database to `version` or the most recent version."""
+    return IMPL.db_sync(version=version, database=database)
+
+
+def db_version(database='main'):
+    """Display the current database version."""
+    return IMPL.db_version(database=database)
+
+
+def db_initial_version(database='main'):
+    """The starting version for the database."""
+    return IMPL.db_initial_version(database=database)
+
+
+def db_null_instance_uuid_scan(delete=False):
+    """Utility for scanning the database to look for NULL instance uuid rows.
+
+    Scans the backing nova database to look for table entries where
+    instances.uuid or instance_uuid columns are NULL (except for the
+    fixed_ips table since that can contain NULL instance_uuid entries by
+    design). Dumps the tables that have NULL instance_uuid entries or
+    optionally deletes them based on usage.
+
+    This tool is meant to be used in conjunction with the 267 database
+    migration script to detect and optionally cleanup NULL instance_uuid
+    records.
+
+    :param delete: If true, delete NULL instance_uuid records found, else
+                   just query to see if they exist for reporting.
+    :returns: dict of table name to number of hits for NULL instance_uuid rows.
+    """
+    return IMPL.db_null_instance_uuid_scan(delete=delete)

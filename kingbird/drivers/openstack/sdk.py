e2da3ef997d17571705af31cbbae0740717a5f56
Ashish Singh <ashishsingh2k8@gmail.com>
2016-01-05 11:57:46 +0530

    Update Cached OS clients for OpenStackDriver
    The cached objects may be out dated over time and needs to be
    updated, Now the cached objects for each region will be updated
    after it has failed to perfrom actions.
    
    Currently it resets/updates cached objects if NotAuthorized, TimeOut
    and ConnectionRefused are thrown from openstack clients.
    Logs the exceptions for other cases.
    
    Change-Id: Ib07292b931c14cdcd080d5bdf956bed105b3c332

--- kingbird/drivers/openstack/sdk.py
+++ kingbird/drivers/openstack/sdk.py
@@ -17,6 +17,11 @@ import collections
 
 from oslo_log import log
 
+from kingbird.common import exceptions
+from kingbird.common.i18n import _
+from kingbird.common.i18n import _LE
+from kingbird.common.i18n import _LI
+
 from kingbird.drivers.openstack.cinder_v2 import CinderClient
 from kingbird.drivers.openstack.keystone_v3 import KeystoneClient
 from kingbird.drivers.openstack.neutron_v2 import NeutronClient
@@ -31,7 +36,9 @@ class OpenStackDriver(object):
 
     def __init__(self, ctx, region_name=None, token=None):
         # Check if objects are cached and try to use those
+        self.region_name = region_name
         if region_name in OpenStackDriver.os_clients_dict:
+            LOG.info(_LI('Using cached OS client objects'))
             self.nova_client = OpenStackDriver.os_clients_dict[
                 region_name]['nova']
             self.cinder_client = OpenStackDriver.os_clients_dict[
@@ -40,9 +47,12 @@ class OpenStackDriver(object):
                 region_name]['neutron']
         else:
             # Create new objects and cache them
+            LOG.debug(_("Creating fresh OS Clients objects"))
             self.nova_client = NovaClient(ctx, region_name, token)
             self.cinder_client = CinderClient(ctx, region_name, token)
             self.neutron_client = NeutronClient(ctx, region_name, token)
+            OpenStackDriver.os_clients_dict[
+                region_name] = collections.defaultdict(dict)
             OpenStackDriver.os_clients_dict[region_name][
                 'nova'] = self.nova_client
             OpenStackDriver.os_clients_dict[region_name][
@@ -56,23 +66,48 @@ class OpenStackDriver(object):
             OpenStackDriver.os_clients_dict['keystone'] = self.keystone_client
 
     def get_enabled_projects(self):
-        return self.keystone_client.get_enabled_projects()
+        try:
+            return self.keystone_client.get_enabled_projects()
+        except Exception as exception:
+            LOG.error(_LE('Error Occurred: %s'), exception.message)
 
     def get_resource_usages(self, project_id):
-        nova_usages = self.nova_client.get_resource_usages(project_id)
-        neutron_usages = self.neutron_client.get_resource_usages(project_id)
-        cinder_usages = self.cinder_client.get_resource_usages(project_id)
-        return nova_usages, neutron_usages, cinder_usages
+        try:
+            nova_usages = self.nova_client.get_resource_usages(project_id)
+            neutron_usages = self.neutron_client.get_resource_usages(
+                project_id)
+            cinder_usages = self.cinder_client.get_resource_usages(project_id)
+            return nova_usages, neutron_usages, cinder_usages
+        except (exceptions.ConnectionRefused, exceptions.NotAuthorized,
+                exceptions.TimeOut):
+            # Delete the cached objects for that region
+            del OpenStackDriver.os_clients_dict[self.region_name]
+        except Exception as exception:
+            LOG.error(_LE('Error Occurred: %s'), exception.message)
 
     def write_quota_limits(self, project_id, limits_to_write):
-        self.nova_client.update_quota_limits(project_id,
-                                             limits_to_write['nova'])
-        self.cinder_client.update_quota_limits(project_id,
-                                               limits_to_write['cinder'])
-        self.neutron_client.update_quota_limits(project_id,
-                                                limits_to_write['neutron'])
+        try:
+            self.nova_client.update_quota_limits(project_id,
+                                                 limits_to_write['nova'])
+            self.cinder_client.update_quota_limits(project_id,
+                                                   limits_to_write['cinder'])
+            self.neutron_client.update_quota_limits(project_id,
+                                                    limits_to_write['neutron'])
+        except (exceptions.ConnectionRefused, exceptions.NotAuthorized,
+                exceptions.TimeOut):
+            # Delete the cached objects for that region
+            del OpenStackDriver.os_clients_dict[self.region_name]
+        except Exception as exception:
+            LOG.error(_LE('Error Occurred: %s'), exception.message)
 
     def delete_quota_limits(self, project_id):
-        self.nova_client.delete_quota_limits(project_id)
-        self.neutron_client.delete_quota_limits(project_id)
-        self.cinder_client.delete_quota_limits(project_id)
+        try:
+            self.nova_client.delete_quota_limits(project_id)
+            self.neutron_client.delete_quota_limits(project_id)
+            self.cinder_client.delete_quota_limits(project_id)
+        except (exceptions.ConnectionRefused, exceptions.NotAuthorized,
+                exceptions.TimeOut):
+            # Delete the cached objects for that region
+            del OpenStackDriver.os_clients_dict[self.region_name]
+        except Exception as exception:
+            LOG.error(_LE('Error Occurred: %s'), exception.message)

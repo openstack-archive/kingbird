26a4d14a69ddf8f2c5487be19a8268f2a94498e4
Ashish Singh <ashishsingh2k8@gmail.com>
2016-02-08 22:23:40 +0530

    Add periodic tasks with oslo.service TGM
    Added periodic task based on TGM timers with configurable periodic
    time interval.
    Periodic quota sync job will be called using this timer.
    Added UTs for the same.
    
    Change-Id: I85fca7b5c563d48a6c244a4acc71892c7b1b1674

--- kingbird/tests/unit/engine/test_listener.py
+++ kingbird/tests/unit/engine/test_listener.py
@@ -10,7 +10,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from kingbird.engine.listener import EngineManager
+import mock
+
+from kingbird.engine import listener
 from kingbird.engine.quota_manager import QuotaManager
 from kingbird.tests import base
 from kingbird.tests import utils
@@ -22,13 +24,13 @@ class TestEngineManager(base.KingbirdTestCase):
         self.context = utils.dummy_context()
 
     def test_init(self):
-        engine_manager = EngineManager()
+        engine_manager = listener.EngineManager()
         self.assertIsNotNone(engine_manager)
         self.assertIsInstance(engine_manager.qm, QuotaManager)
 
     def test_say_hello_world_call(self):
         payload = "test payload"
-        engine_manager = EngineManager()
+        engine_manager = listener.EngineManager()
         return_value = engine_manager.say_hello_world_call(self.context,
                                                            payload)
         expected_output = "payload: %s" % payload
@@ -36,8 +38,18 @@ class TestEngineManager(base.KingbirdTestCase):
 
     def test_say_hello_world_cast(self):
         payload = "test payload"
-        engine_manager = EngineManager()
+        engine_manager = listener.EngineManager()
         return_value = engine_manager.say_hello_world_call(self.context,
                                                            payload)
         expected_output = "payload: %s" % payload
         self.assertEqual(return_value, expected_output)
+
+    @mock.patch.object(listener, 'context')
+    @mock.patch.object(listener, 'QuotaManager')
+    def test_periodic_balance_all(self, mock_qm, mock_context):
+        engine_manager = listener.EngineManager()
+        cntxt = utils.dummy_context()
+        mock_context.get_admin_context().return_value = cntxt
+        engine_manager.periodic_balance_all()
+        mock_qm().periodic_balance_all.assert_called_once_with(
+            mock_context.get_admin_context())

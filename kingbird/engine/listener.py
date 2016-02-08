0b2f7ae258d0f35c9dede924d2e3e3505949dfa7
Ashish Singh <ashishsingh2k8@gmail.com>
2016-02-08 22:23:40 +0530

    Add periodic tasks with oslo.service TGM
    Added periodic task based on TGM timers with configurable periodic
    time interval.
    Periodic quota sync job will be called using this timer.
    Added UTs for the same.
    
    Change-Id: I85fca7b5c563d48a6c244a4acc71892c7b1b1674

--- kingbird/engine/listener.py
+++ kingbird/engine/listener.py
@@ -16,15 +16,31 @@
 from oslo_config import cfg
 from oslo_log import log as logging
 import oslo_messaging as messaging
+import time
 
 from kingbird.common.i18n import _
 from kingbird.common.i18n import _LI
+from kingbird.common import context
 from kingbird.common import manager
 from kingbird.engine.quota_manager import QuotaManager
+from kingbird.engine import scheduler
+
 
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
+scheduler_opts = [
+      cfg.BoolOpt('periodic_enable',
+                 default=True,
+                 help='boolean value for enable/disenable periodic tasks'),
+      cfg.IntOpt('periodic_interval',
+                 default=100,
+                 help='periodic time interval for automatic quota sync job')
+]
+
+scheduler_opt_group = cfg.OptGroup('scheduler')
+cfg.CONF.register_group(scheduler_opt_group)
+cfg.CONF.register_opts(scheduler_opts, group=scheduler_opt_group)
 
 class EngineManager(manager.Manager):
     """Manages all the kb engine activities."""
@@ -33,10 +49,17 @@ class EngineManager(manager.Manager):
 
     def __init__(self, *args, **kwargs):
         self.qm = QuotaManager()
+        self.TG = scheduler.ThreadGroupManager()
+        self.periodic_enable = cfg.CONF.scheduler.periodic_enable
+        self.periodic_interval = cfg.CONF.scheduler.periodic_interval
         LOG.debug(_('Engine initialization...'))
 
         super(EngineManager, self).__init__(service_name="engine_manager",
                                             *args, **kwargs)
+        if self.periodic_enable:
+            LOG.debug("Adding periodic tasks for the engine to perform")
+            self.TG.add_timer(self.periodic_interval,
+                              self.periodic_balance_all)
 
     def init_host(self):
         LOG.debug(_('Engine init_host...'))
@@ -73,3 +96,8 @@ class EngineManager(manager.Manager):
         # no return value to browser indeed for cast. check the log info
         info_text = "payload: %s" % payload
         return {'engine': info_text}
+
+    def periodic_balance_all(self):
+        LOG.info(_LI("Periodic quota sync job started at: %s"), time.strftime("%c"))
+        ctxt = context.get_admin_context()
+        self.qm.periodic_balance_all(ctxt)

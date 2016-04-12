4edf21f00bd212eb8d9adc9a28068b422bfe68ce
Chaoyi Huang <joehuang@huawei.com>
2016-04-12 16:56:47 +0800

    List resource controller after in V1Controller
    The resource controller routing link should be able to be
    returned when issue http://host:port/v1.0
    
    Change-Id: Idb7a9839d0e5be2a568fcdae0519dbe8f9bdf592
    Signed-off-by: Chaoyi Huang <joehuang@huawei.com>

--- kingbird/api/controllers/root.py
+++ kingbird/api/controllers/root.py
@@ -23,10 +23,6 @@ class RootController(object):
 
     @pecan.expose('json')
     def _lookup(self, version, *remainder):
-        if remainder:
-            if remainder[1] != 'os-quota-sets' or len(remainder) < 3:
-                    pecan.abort(405)
-            remainder = remainder[1:]
         if version == 'v1.0':
             return V1Controller(), remainder
 
@@ -62,11 +58,26 @@ class V1Controller(object):
     def __init__(self):
 
         self.sub_controllers = {
-            "os-quota-sets": quota_manager.QuotaManagerController()
+            "os-quota-sets": quota_manager.QuotaManagerController,
         }
 
         for name, ctrl in self.sub_controllers.items():
             setattr(self, name, ctrl)
+
+    def _get_resource_controller(self, tenant_id, remainder):
+        if not remainder:
+            pecan.abort(404)
+            return
+        resource = remainder[0]
+        if resource not in self.sub_controllers:
+            pecan.abort(404)
+            return
+
+        return self.sub_controllers[resource](), remainder[1:]
+
+    @pecan.expose()
+    def _lookup(self, tenant_id, *remainder):
+        return self._get_resource_controller(tenant_id, remainder)
 
     @pecan.expose(generic=True, template='json')
     def index(self):
@@ -77,7 +88,8 @@ class V1Controller(object):
                  "href": pecan.request.application_url + "/v1.0"}
             ] + [
                 {"rel": name,
-                 "href": pecan.request.application_url + "/v1.0/" + name}
+                 "href": pecan.request.application_url +
+                         "/v1.0/{tenant_id}/" + name}
                 for name in sorted(self.sub_controllers)
             ]
         }

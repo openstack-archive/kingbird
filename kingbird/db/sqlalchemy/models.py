8d2aa2f6a5b4b059e0a33bce84702b975bc4ca24
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2016-05-02 15:16:06 +0200

    Add Quota class data model
    In order to fully support quota we need quota classes. Added
    implementation of QuotaClass model as well as db api methods for
    creating/updating/deleting quota classes.
    
    Change-Id: I24278b12a7242ece8500a3800bb42904fd9aafc2

--- kingbird/db/sqlalchemy/models.py
+++ kingbird/db/sqlalchemy/models.py
@@ -20,7 +20,7 @@ from oslo_config import cfg
 from oslo_db.sqlalchemy import models
 
 from sqlalchemy.orm import session as orm_session
-from sqlalchemy import (Column, Integer, String)
+from sqlalchemy import (Column, Integer, String, schema)
 from sqlalchemy.ext.declarative import declarative_base
 
 CONF = cfg.CONF
@@ -66,15 +66,48 @@ class KingbirdBase(models.ModelBase,
 
 
 class Quota(BASE, KingbirdBase):
+    """Represents a single quota override for a project.
+
+    If there is no row for a given project id and resource, then the
+    default for the quota class is used.  If there is no row for a
+    given quota class and resource, then the default for the
+    deployment is used. If the row is present but the hard limit is
+    Null, then the resource is unlimited.
+    """
+
     __tablename__ = 'quotas'
+
+    __table_args__ = (
+        schema.UniqueConstraint("project_id", "resource", "deleted",
+                                name="uniq_quotas0project_id0resource0deleted"
+                                ),)
 
     id = Column(Integer, primary_key=True)
 
-    project_id = Column(String(36))
+    project_id = Column(String(255))
 
     resource = Column(String(255), nullable=False)
 
     hard_limit = Column(Integer, nullable=False)
+
+
+class QuotaClass(BASE, KingbirdBase):
+    """Represents a single quota override for a quota class.
+
+    If there is no row for a given quota class and resource, then the
+    default for the deployment is used.  If the row is present but the
+    hard limit is Null, then the resource is unlimited.
+    """
+
+    __tablename__ = "quota_classes"
+
+    id = Column(Integer, primary_key=True)
+
+    class_name = Column(String(255), nullable=False)
+
+    resource = Column(String(255), nullable=False)
+
+    hard_limit = Column(Integer, nullable=True)
 
 
 class SyncLock(BASE, KingbirdBase):

70c8d0704205859bb1d55f369ad9fc7032e601ba
Ashish Singh <ashishsingh2k8@gmail.com>
2016-05-27 22:32:19 +0530

    Kingbird Refactor(Part 2/Final):
    Add configuration for auto discovery of kingbird Tempest testcases.
    
    Restructure and fixed minor issues in existing tempest.
    
    Added README.rst for the steps to run Kingbird tempest plugin.
    
    The configurations which are specific to kingbird
    plugin are in kingbird/tests/tempest/scenario/config.py and
    currently the default configuration is used from here.
    
    Change-Id: I1b652c82beea61d84b3f42bf657925b1571f7a8d

--- kingbird/tests/tempest/scenario/quota_management/client_tests/base.py
+++ kingbird/tests/tempest/scenario/quota_management/client_tests/base.py
@@ -1,4 +1,4 @@
-# Copyright 2012 OpenStack Foundation
+# Copyright 2016 Ericsson AB
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,27 +12,21 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 import collections
 import time
 
-from tempest.common import kingbird
 from tempest import config
 from tempest.lib.common import api_version_utils
 from tempest.lib.common.utils import data_utils
 import tempest.test
 
+from kingbird.tests.tempest.scenario import consts
+from kingbird.tests.tempest.scenario.quota_management import sync_client
+
 CONF = config.CONF
 Global_instance_limit = 10
-DEFAULT_QUOTAS = {
-    u'quota_set': {
-        u'metadata_items': 128, u'subnet': 10, u'consistencygroups': 10,
-        u'floatingip': 50, u'gigabytes': 1000, u'backup_gigabytes': 1000,
-        u'ram': 51200, u'floating_ips': 10, u'snapshots': 10,
-        u'instances': 10, u'key_pairs': 100, u'volumes': 10, u'router': 10,
-        u'security_group': 10, u'cores': 20, u'backups': 10, u'fixed_ips': -1,
-        u'port': 50, u'security_groups': 10, u'network': 10
-        }
-    }
+DEFAULT_QUOTAS = consts.DEFAULT_QUOTAS
 # Time to wait for sync to finish
 TIME_TO_SYNC = CONF.kingbird.TIME_TO_SYNC
 
@@ -44,17 +38,14 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
     @classmethod
     def skip_checks(cls):
         super(BaseKingbirdTest, cls).skip_checks()
-        if not CONF.service_available.kingbird:
-            raise cls.skipException("Kingbird is not available")
-        # import config variables
 
     @classmethod
     def setup_credentials(cls):
         super(BaseKingbirdTest, cls).setup_credentials()
-        session = kingbird.get_session()
+        session = sync_client.get_session()
         cls.auth_token = session.get_token()
-        cls.key_client = kingbird.get_key_client(session)
-        cls.regions = kingbird.get_regions(cls.key_client)
+        cls.key_client = sync_client.get_key_client(session)
+        cls.regions = sync_client.get_regions(cls.key_client)
 
     @classmethod
     def setup_clients(cls):
@@ -64,16 +55,16 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
     def resource_setup(cls):
         super(BaseKingbirdTest, cls).resource_setup()
         # Create Project, User, flavor, subnet & network for test
-        project_name = data_utils.rand_name(__name__ + '-project')
-        user_name = data_utils.rand_name(__name__ + '-user')
-        password = data_utils.rand_name(__name__ + '-password')
-        openstack_details = kingbird.get_openstack_drivers(cls.key_client,
-                                                           cls.regions[0],
-                                                           project_name,
-                                                           user_name,
-                                                           password)
+        project_name = data_utils.rand_name('kb-project')
+        user_name = data_utils.rand_name('kb-user')
+        password = data_utils.rand_name('kb-password')
+        openstack_details = sync_client.get_openstack_drivers(cls.key_client,
+                                                              cls.regions[0],
+                                                              project_name,
+                                                              user_name,
+                                                              password)
         cls.openstack_drivers = openstack_details['os_drivers']
-        cls.resource_ids = kingbird.create_resources(cls.openstack_drivers)
+        cls.resource_ids = sync_client.create_resources(cls.openstack_drivers)
         cls.resource_ids.update(openstack_details)
         cls.session = openstack_details['session']
 
@@ -83,61 +74,61 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
         default_quota = {'instances': DEFAULT_QUOTAS['quota_set']['instances'],
                          'cores': DEFAULT_QUOTAS['quota_set']['cores'],
                          'ram': DEFAULT_QUOTAS['quota_set']['ram']}
-        cls.set_default_quota(CONF.kingbird.project_id, default_quota)
-        kingbird.resource_cleanup(cls.openstack_drivers, cls.resource_ids)
-        kingbird.delete_custom_kingbird_quota(
-            cls.auth_token, CONF.kingbird.project_id, None)
+        cls.set_default_quota(cls.resource_ids['project_id'], default_quota)
+        sync_client.resource_cleanup(cls.openstack_drivers, cls.resource_ids)
+        sync_client.delete_custom_kingbird_quota(
+            cls.auth_token, cls.resource_ids['project_id'], None)
 
     def setUp(self):
         super(BaseKingbirdTest, self).setUp()
 
     @classmethod
     def create_custom_kingbird_quota(cls, project_id, new_quota_values):
-        new_values = kingbird.create_custom_kingbird_quota(
+        new_values = sync_client.create_custom_kingbird_quota(
             cls.auth_token, project_id, new_quota_values)
         return new_values
 
     @classmethod
     def get_custom_kingbird_quota(cls, project_id):
-        return_quotas = kingbird.get_custom_kingbird_quota(
+        return_quotas = sync_client.get_custom_kingbird_quota(
             cls.auth_token, project_id)
         return return_quotas
 
     @classmethod
     def delete_custom_kingbird_quota(cls, project_id, quota_to_delete=None):
-        deleted_quotas = kingbird.delete_custom_kingbird_quota(
+        deleted_quotas = sync_client.delete_custom_kingbird_quota(
             cls.auth_token, project_id, quota_to_delete)
         return deleted_quotas
 
     @classmethod
     def get_default_kingbird_quota(cls):
-        return_quotas = kingbird.get_default_kingbird_quota(cls.auth_token)
+        return_quotas = sync_client.get_default_kingbird_quota(cls.auth_token)
         return return_quotas
 
     @classmethod
     def quota_sync_for_project(cls, project_id):
-        sync_status = kingbird.quota_sync_for_project(
+        sync_status = sync_client.quota_sync_for_project(
             cls.auth_token, project_id)
         return sync_status
 
     @classmethod
     def get_quota_usage_for_project(cls, project_id):
-        quota_usage = kingbird.get_quota_usage_for_project(
+        quota_usage = sync_client.get_quota_usage_for_project(
             cls.auth_token, project_id)
         return quota_usage
 
     @classmethod
     def create_custom_kingbird_quota_wrong_token(cls, project_id,
                                                  new_quota_values):
-        new_values = kingbird.create_custom_kingbird_quota_wrong_token(
+        new_values = sync_client.create_custom_kingbird_quota_wrong_token(
             cls.auth_token, project_id, new_quota_values)
         return new_values
 
     @classmethod
     def create_instance(cls, count=1):
         try:
-            server_ids = kingbird.create_instance(cls.openstack_drivers,
-                                                  cls.resource_ids, count)
+            server_ids = sync_client.create_instance(cls.openstack_drivers,
+                                                     cls.resource_ids, count)
         except Exception as e:
             server_ids = {'server_ids': list(e.args)}
             raise
@@ -146,13 +137,13 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
 
     @classmethod
     def delete_instance(cls):
-        kingbird.delete_instance(cls.openstack_drivers, cls.resource_ids)
+        sync_client.delete_instance(cls.openstack_drivers, cls.resource_ids)
         cls.resource_ids['instances'] = None
 
     @classmethod
     def calculate_quota_limits(cls, project_id):
         calculated_quota_limits = collections.defaultdict(dict)
-        resource_usage = kingbird.get_usage_from_os_client(
+        resource_usage = sync_client.get_usage_from_os_client(
             cls.session, cls.regions, project_id)
         total_usages = cls.get_summation(resource_usage)
         for current_region in cls.regions:
@@ -177,14 +168,14 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
 
     @classmethod
     def get_usage_manually(cls, project_id):
-        resource_usage = kingbird.get_usage_from_os_client(
+        resource_usage = sync_client.get_usage_from_os_client(
             cls.session, cls.regions, project_id)
         resource_usage = cls.get_summation(resource_usage)
         return {'quota_set': resource_usage}
 
     @classmethod
     def get_actual_limits(cls, project_id):
-        actual_limits = kingbird.get_actual_limits(
+        actual_limits = sync_client.get_actual_limits(
             cls.session, cls.regions, project_id)
         return actual_limits
 
@@ -194,5 +185,5 @@ class BaseKingbirdTest(api_version_utils.BaseMicroversionTest,
 
     @classmethod
     def set_default_quota(cls, project_id, quota_to_set):
-        kingbird.set_default_quota(
+        sync_client.set_default_quota(
             cls.session, cls.regions, project_id, **quota_to_set)

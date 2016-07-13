675c9d6dc1775906dd4c14600e56d0c58aef56a8
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2016-07-13 14:44:59 +0200

    Kill running kingbird processes before starting new ones
    Change-Id: Ifa39fc26473076cf16d20cba33c36f128c0177e0

--- tools/opnfv/install_kingbird.sh
+++ tools/opnfv/install_kingbird.sh
@@ -157,8 +157,11 @@ iniset $KINGBIRD_CONF_FILE database connection "mysql://$mysql_user:$mysql_pass@
 iniset $KINGBIRD_CONF_FILE database max_overflow -1
 iniset $KINGBIRD_CONF_FILE database max_pool_size 1000
 
-# Run kingbird
+# Kill kingbird
+pkill -f kingbird-api
+pkill -f kingbird-engine
 
+# Run kingbird
 mkdir -p /var/log/kingbird
 kingbird-manage --config-file $KINGBIRD_CONF_FILE db_sync
 nohup kingbird-engine --config-file $KINGBIRD_CONF_FILE --log-file /var/log/kingbird/kingbird-engine.log &

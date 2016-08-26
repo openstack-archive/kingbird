cc4b8a9bb4597951824f39081d92b078ee0bbbfc
Ashish Singh <ashishsingh2k8@gmail.com>
2016-08-26 16:31:46 +0530

    Fix quota get for non admin role user
    There were minor issues with context object
    manipulation which was raising exception during
    db access with non admin role users, fixed it.
    
    Change-Id: I8bf2b272b6feefc989a390e7b7869dbb58386534
    Closes-Bug: #1610610

--- kingbird/db/sqlalchemy/api.py
+++ kingbird/db/sqlalchemy/api.py
@@ -100,7 +100,7 @@ def is_user_context(context):
         return False
     if context.is_admin:
         return False
-    if not context.user or not context.tenant_id:
+    if not context.user or not context.tenant:
         return False
     return True
 

2798ae8ad6f2e9e097099a5d1c64e89f0a9c7c96
ubuntu <pratapagoutham@gmail.com>
2017-01-30 18:11:11 +0530

    Migration_Scripts for Resource_sync.
    Create two tables sync_job and resource_sync.
    Sync_job contains job details of which id is the primary-key.
    id is taken as a foreign-key and is also a part of composite primary key
    along with Resource,Region off the resource_sync table.
    Also created the api-calls with which interaction between the
    application and the database occurs.
    Added Test_cases for the same.
    
    Change-Id: Iba1c789715aa7c7d00a7a2ec23a449c72a757ac1

---
+++ kingbird/db/sqlalchemy/migrate_repo/versions/006_sync_job.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2017 Ericsson AB.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import sqlalchemy
+
+
+def upgrade(migrate_engine):
+    meta = sqlalchemy.MetaData()
+    meta.bind = migrate_engine
+
+    sync_job = sqlalchemy.Table(
+        'sync_job', meta,
+        sqlalchemy.Column('id', sqlalchemy.String(36),
+                          primary_key=True),
+        sqlalchemy.Column('sync_status', sqlalchemy.String(length=255),
+                          nullable=False),
+        sqlalchemy.Column('user_id', sqlalchemy.String(36),
+                          nullable=False),
+        sqlalchemy.Column('tenant_id', sqlalchemy.String(36),
+                          nullable=False),
+        sqlalchemy.Column('updated_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('created_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('deleted_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('deleted', sqlalchemy.Integer),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+    )
+
+    resource_sync = sqlalchemy.Table(
+        'resource_sync', meta,
+        sqlalchemy.Column('job_id', sqlalchemy.String(36),
+                          sqlalchemy.ForeignKey('sync_job.id'),
+                          primary_key=True),
+        sqlalchemy.Column('region', sqlalchemy.String(36),
+                          primary_key=True),
+        sqlalchemy.Column('resource', sqlalchemy.String(36),
+                          primary_key=True),
+        sqlalchemy.Column('resource_type', sqlalchemy.String(36),
+                          nullable=False),
+        sqlalchemy.Column('sync_status', sqlalchemy.String(36),
+                          nullable=False),
+        sqlalchemy.Column('updated_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('created_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('deleted_at', sqlalchemy.DateTime),
+        sqlalchemy.Column('deleted', sqlalchemy.Integer),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+    )
+    sync_job.create()
+    resource_sync.create()

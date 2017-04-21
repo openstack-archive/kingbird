8d1de6ed860f34b75dcc7a418df4ea62c15af1c1
joehuang <joehuang@huawei.com>
2017-04-21 02:37:23 -0400

    Enable some tempest test cases
    Enable tempest test cases for quota management and resourece
    management as example
    
    Change-Id: I9f5f0be8cbef26a51d035e5829e225fe1cce2f7a
    Signed-off-by: joehuang <joehuang@huawei.com>

--- kingbird/tests/tempest/post_test_hook.sh
+++ kingbird/tests/tempest/post_test_hook.sh
@@ -17,8 +17,8 @@
 export DEST=$BASE/new
 export DEVSTACK_DIR=$DEST/devstack
 export KINGBIRD_DIR=$DEST/kingbird
-export KINGBIRD_DEVSTACK_PLUGIN_DIR=$TRICIRCLE_DIR/devstack
-export KINGBIRD_TEMPEST_PLUGIN_DIR=$TRICIRCLE_DIR/kingbird/tests/tempest
+export KINGBIRD_DEVSTACK_PLUGIN_DIR=$KINGBIRD_DIR/devstack
+export KINGBIRD_TEMPEST_PLUGIN_DIR=$KINGBIRD_DIR/kingbird/tests/tempest
 export TEMPEST_DIR=$DEST/tempest
 export TEMPEST_CONF=$TEMPEST_DIR/etc/tempest.conf
 
@@ -27,9 +27,11 @@ if [ "$OS_REGION_NAME" -ne "RegionOne" ]; then
     return 0
 fi
 
+cd $KINGBIRD_DIR
+sudo pip install -e .
+
 PRIMARY_NODE_IP=$(cat /etc/nodepool/primary_node_private)
 
-# use admin role to create Tricircle top Pod and Pod1
 source $DEVSTACK_DIR/openrc admin admin
 unset OS_REGION_NAME
 
@@ -37,6 +39,10 @@ mytoken=$(openstack --os-region-name=RegionOne token issue | awk 'NR==5 {print $
 echo $mytoken
 
 image_id=$(openstack --os-region-name=RegionOne image list | awk 'NR==4 {print $2}')
+alt_image_id=$(openstack --os-region-name=RegionOne image list | awk 'NR==5 {print $2}')
+
+flavor_id=$(openstack --os-region-name=RegionOne flavor list | awk 'NR==4 {print $2}')
+alt_flavor_id=$(openstack --os-region-name=RegionOne flavor list | awk 'NR==5 {print $2}')
 
 # preparation for the tests
 cd $TEMPEST_DIR
@@ -56,12 +62,17 @@ source $DEVSTACK_DIR/functions
 iniset $TEMPEST_CONF auth admin_username ${ADMIN_USERNAME:-"admin"}
 iniset $TEMPEST_CONF auth admin_project_name admin
 iniset $TEMPEST_CONF auth admin_password $OS_PASSWORD
-iniset $TEMPEST_CONF identity uri $OS_AUTH_URL
-iniset $TEMPEST_CONF identity-feature-enabled api_v3 false
+iniset $TEMPEST_CONF auth admin_domain_name default
+iniset $TEMPEST_CONF identity auth_version v2
+iniset $TEMPEST_CONF identity uri_v3 http://$SERVICE_HOST/identity/v3
+iniset $TEMPEST_CONF identity uri http://$SERVICE_HOST:5000/v2.0/
+iniset $TEMPEST_CONF identity-feature-enabled api_v2 True
 
 iniset $TEMPEST_CONF compute region RegionOne
 iniset $TEMPEST_CONF compute image_ref $image_id
-iniset $TEMPEST_CONF compute image_ref_alt $image_id
+iniset $TEMPEST_CONF compute image_ref_alt $alt_image_id
+iniset $TEMPEST_CONF compute flavor_ref $flavor_id
+iniset $TEMPEST_CONF compute flavor_ref_alt $alt_flavor_id
 
 iniset $TEMPEST_CONF volume region RegionOne
 iniset $TEMPEST_CONF volume catalog_type volumev2
@@ -70,6 +81,7 @@ iniset $TEMPEST_CONF volume-feature-enabled api_v1 false
 
 iniset $TEMPEST_CONF validation connect_method fixed
 
+export TEMPEST_CONF=$TEMPEST_DIR/etc/tempest.conf
 
 # Run the Tempest tests through ostestr command
 # preparation for the tests
@@ -78,7 +90,7 @@ cd $TEMPEST_DIR
 
 # ping kingbird api
 
-if curl -s --head  --request GET http://$PRIMARY_NODE_IP:8118 | grep "200 OK" > /dev/null; then 
+if curl -s --head  --request GET http://$PRIMARY_NODE_IP:8118 | grep "200 OK" > /dev/null; then
    echo "kb-api is UP"
 else
    echo "kb-api is DOWN"
@@ -90,3 +102,24 @@ echo "start Kingbird multi-region test..."
 # specify what kingbird test cases to be tested in TESTCASES environment
 # variables, then uncomment the follow line
 # ostestr --regex $TESTCASES
+
+# TESTCASES="(scenario.quota_management.client_tests.test_quota_class_api"
+# TESTCASES="$TESTCASES|scenario.quota_management.client_tests.test_quota_management_api"
+# TESTCASES="$TESTCASES|scenario.resource_management.sync_tests.test_keypair_sync_api"
+# TESTCASES="$TESTCASES)"
+
+# ostestr --regex $TESTCASES
+
+testr init
+
+sudo pip install entry_point_inspector
+
+epi group show tempest.test_plugins
+
+testr list-tests | grep Kingbird
+
+testr run scenario.quota_management.client_tests.test_quota_management_api
+
+testr run scenario.quota_management.client_tests.test_quota_class_api
+
+testr run scenario.resource_management.sync_tests.test_keypair_sync_api

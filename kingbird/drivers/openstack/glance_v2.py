91b79b17450e3bf08eba8470b39d235fafa6e2dd
Goutham Pratapa <pratapagoutham@gmail.com>
2017-06-09 18:17:03 +0530

    Implement Image synchronization in Kingbird.
    Image-synchronization syncs multiple images from one region to
    multiple target regions.
    Currently the supported image disk_formats are Qcow2 and AMI.
    If user triggers a request to sync AMI format image then Kingbird
    syncs the dependent AKI and ARI images as well.
    -Verification of Dependent images is handled by glance_adapter
    -Modified sync_manager to keypair_sync_manager and made it an
    independent entity.
    -Created an independent Image Sync-manager which syncs images in
    multiple target regions.
    Added Test-cases for the same.
    Depends-on: I316ff7e79afb0ea092eec2a4bf32a78c609198ab
    Implements: blueprint image-synchronization
    
    Change-Id: If207f803b67b5f5702117d377883b57101802c42

--- kingbird/drivers/openstack/glance_v2.py
+++ kingbird/drivers/openstack/glance_v2.py
@@ -75,6 +75,7 @@ class GlanceClient(object):
             have to be retrieved.
 
         """
+        LOG.info("Checking for image in source_region.")
         try:
             image = self.glance_client.images.get(resource_identifier)
             LOG.info("Source image: %s", image.name)
@@ -82,3 +83,89 @@ class GlanceClient(object):
         except Exception as exception:
             LOG.error('Exception Occurred: %s', exception.message)
             pass
+
+    def get_image(self, resource_identifier):
+        """Get the image details for the specified resource_identifier.
+
+        :param resource_identifier: resource_id for which the details
+            have to be retrieved.
+
+        """
+        LOG.info("Fetching image: %s", resource_identifier)
+        return self.glance_client.images.get(resource_identifier)
+
+    def get_image_data(self, resource_identifier):
+        """Get the image data of the specified resource.
+
+        :param resource_identifier: resource_id for which the details
+            have to be retrieved.
+
+        """
+        LOG.info("Fetching data: %s", resource_identifier)
+        return self.glance_client.images.data(resource_identifier)
+
+    def create_image(self, image, force, kernel_image=None,
+                     ramdisk_image=None):
+        """Create image with the same properties of the source image.
+
+        :param kwargs: properties of the image to create in target
+            region.
+        """
+        LOG.warning('Image with id same as that of source_image will be'
+                    ' created.If any issue while syncing an image'
+                    ' It is because the id already has an entry in'
+                    ' target region use --force to avoid this issue.')
+        kwargs = {
+            "min_ram": image.min_ram,
+            "protected": image.protected,
+            "min_disk": image.min_disk,
+            "name": image.name,
+            "visibility": image.visibility,
+            "tags": image.tags,
+            "owner": image.owner,
+            "container_format": image.container_format,
+            "disk_format": image.disk_format,
+            "id": image.id
+        }
+        if force:
+            LOG.info("Image with a new-id is created.")
+            kwargs.pop('id')
+        if hasattr(image, 'architecture'):
+            kwargs["architecture"] = image.architecture
+        if hasattr(image, 'os_version'):
+            kwargs["os_version"] = image.os_version
+        if hasattr(image, 'os_distro'):
+            kwargs["os_distro"] = image.os_distro
+        if kernel_image:
+            kwargs["kernel-id"] = kernel_image
+        if ramdisk_image:
+            kwargs["ramdisk-id"] = ramdisk_image
+        LOG.info("Creating Image: %s", image.name)
+        return self.glance_client.images.create(**kwargs)
+
+    def image_upload(self, image_id, image_data):
+        """Upload image to glance.
+
+        :param image_id: UUID of the image to be uploaded.
+        """
+        LOG.info("Uploading image: %s", image_id)
+        return self.glance_client.images.upload(image_id, image_data)
+
+
+class GlanceUpload(object):
+    def __init__(self, data):
+        self.received = data
+
+    def read(self, chunk_size):
+        """Replace the actual read method in glance.
+
+        Please note that we are using this to replace the actual
+        read in glance.
+        when we download an image a chunk of 65536 will be written into
+        the destination file and
+        when we upload an image using a file it reads the entire file
+        in the form of chunks(65536 KB). So our approach is to get
+        entire imagedata into an iterator and send this 65536kb chunk to
+        the glance image upload and there by omitting the usage of file.
+        """
+        return self.received.next()

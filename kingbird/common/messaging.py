f161aabf4a88666fd35ce47e3495ed1bcf98251b
shubhendu <shubhendu.poothia@nectechnologies.in>
2017-06-15 14:13:40 +0530

    Set access_policy for messaging's dispatcher
    oslo.messaging allow dispatcher to restrict endpoint methods
    since 5.11.0 in d3a8f280ebd6fd12865fd20c4d772774e39aefa2, set with
    DefaultRPCAccessPolicy to fix FutureWarning like:
    
    FutureWarning: The access_policy argument is changing its default
    value to <class 'oslo_messaging.rpc.dispatcher.DefaultRPCAccessPolicy'>
    in version '?', please update the code to explicitly set None as the
    value: access_policy defaults to LegacyRPCAccessPolicy which exposes
    private methods. Explicitly set access_policy to DefaultRPCAccessPolicy
    or ExplicitRPCAccessPolicy.
    
    Change-Id: I9b21b6aa77038f59fd5db3a198ed4928502b785f

--- kingbird/common/messaging.py
+++ kingbird/common/messaging.py
@@ -14,6 +14,7 @@ import eventlet
 
 from oslo_config import cfg
 import oslo_messaging
+from oslo_messaging.rpc import dispatcher
 from oslo_serialization import jsonutils
 
 from kingbird.common import context
@@ -93,9 +94,11 @@ def cleanup():
 def get_rpc_server(target, endpoint):
     """Return a configured oslo_messaging rpc server."""
     serializer = RequestContextSerializer(JsonPayloadSerializer())
+    access_policy = dispatcher.DefaultRPCAccessPolicy
     return oslo_messaging.get_rpc_server(TRANSPORT, target, [endpoint],
                                          executor='eventlet',
-                                         serializer=serializer)
+                                         serializer=serializer,
+                                         access_policy=access_policy)
 
 
 def get_rpc_client(**kwargs):
